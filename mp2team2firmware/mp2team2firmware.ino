827d3439d7d0e7e1915d030e65a55fdcdf139acc
hurricaneJoef <hurricane.joef@gmail.com>
2022-09-16 10:45:07 -0400

    slight edits

--- mp2team2firmware/mp2team2firmware.ino
+++ mp2team2firmware/mp2team2firmware.ino
@@ -8,30 +8,38 @@
 #define prereadDelay 40
 #define readDelay 20
 #define readSamples 5
-#define outliersRemoved 1
 
-
+#define baud 9600
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(baud);
+  delay(1000);
+  Serial.println(F("pie mp2 scanner"));
 }
 
 int readDistNow(){
   int dataPoints[readSamples];
-  int maxPoints[outliersRemoved];
-  int minPoints[outliersRemoved];
-  delay(prereadDelay 40);
-  for(uint8_t i=0;i<readsamples;i++){
+  delay(prereadDelay);
+  for(uint8_t i=0;i<readSamples;i++){
     dataPoints[i] = analogRead(distPin);
     delay(readDelay);
   }
+  int rawDist = QuickMedian<int>::GetMedian(dataPoints, readSamples);
+  int dist = convertFromRaw(rawDist);
+  return(dist);
   
+}
+
+int convertFromRaw(int raw){
+  return(raw); // TODO just a pass through rn
 }
 
 
 
 void loop() {
+  Serial.println(readDistNow());
+  delay(1000);
   // put your main code here, to run repeatedly:
 
 }
