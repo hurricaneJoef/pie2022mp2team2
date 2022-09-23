47f364dbb3acf3df25b288e1118e2e1f670f3185
Arturo Joya <98101322+ArturoJoya@users.noreply.github.com>
2022-09-22 21:16:10 -0400

    Fixed Scan Results
    Fixed the way x,y,and z are calculated

--- mp2team2software/arduino3DScanner.m
+++ mp2team2software/arduino3DScanner.m
@@ -1,17 +1,21 @@
 % File designed to collect data from the Pan-Tilt Mechanism servos and the
 % IR sensor that scans the geometry of an object.
+
+
 function arduino3DScanner(logfile,doScan)
-    function x = get_x_pos(phi,theta,r)
-        x = phi;%r*sin(phi)*cos(theta);
+    spacing = 5;
+    function [x,y,z] = get_coords(phi,theta,r)
+        phi = 45-phi;
+        theta = 45-theta;
+        x= (r*cosd(theta)*sind(phi));
+        z= r*cosd(theta)*cosd(phi);
+        y= -r*sind(theta);
     end
-    function y = get_y_pos(phi,theta,r)
-        y = theta;%r*cos(phi)*sin(theta);
-    end
-    function z = get_z_val(theta,r)
-        %r = r*sin(theta)/4;
-        z = uint8(r);
-    end
-    function handleKeyPress(fig,event) %function to stop and save a scan with the spacebar, p to save
+    %the function below was adapted from QEA3s
+    %arduinoLibraryConnectionTherma to act as a 'stop' in case of an
+    %emergency.
+    function handleKeyPress(fig,event) 
+        %function to stop and save a scan with the spacebar, p to save
         if strcmp(event.Key, 'space') == 1 && strcmp(event.EventName, 'KeyPress') == 1
             shouldStop = true;
         end
@@ -32,48 +36,42 @@ function arduino3DScanner(logfile,doScan)
     if nargin < 2
         doScan = true; % turn on the scan once we tell it to on the command window
     end
+    
 clear s
 f = figure(); % start the figure for where we'll see the XYZ coordinates
 set(f,'KeyPressFcn',@handleKeyPress); % look out for the press that that will finalize the figure
-%a = arduino(); %set up arduino
-port = 'COM6';%a.Port;
-%clear a
-%a = arduino(port);
+%establish a hardcoded serial connection with the arduino.
+port = 'COM6';
 baudrate = 9600;
 s = serialport(port,baudrate);
 data = [0,0,0];
-%C = zeros(90);
 shouldStop = false; 
 figure(f);
-pause(4);
-timeout = 0;
+pause(4); % wait to ensure the connection is 
+writeline(s, '45,45')
+pause(4)
 while ~shouldStop
-for phi = 10:2:80            %sweep through servo angles
-    for theta = 10:2:80
+%sweep through the servo angles we'll be using
+for phi = 10:spacing:80
+    for theta = 10:spacing:80
         coord = strcat(num2str(phi),',',num2str(theta));
         disp(strcat('sending ',coord))
         writeline(s, coord) %make the arduino reach a position
         timeout = 0;
         ready = 0;
-        while timeout < 20 && ~ready
+        while timeout < 20 && ~ready %breaks once a signal is received and is within the timeout threshold
             while s.NumBytesAvailable > 0
                 read = readline(s);
                 ready = 1;
                 vals = split(read, ',');
-                r = str2double(vals(3)); %read IR data for the phi,theta
-                x = get_x_pos(phi,theta,r);
-                y = get_y_pos(phi,theta,r);
-                z = get_z_val(theta,r);
+                r = (str2double(vals(3))/16)+2.25; %read IR data for the phi,theta
+                [x,y,z] = get_coords(phi,theta,r);
                 data = [data;[x,y,z]]; %store values in a matrix
-                %C(phi,theta) = z; %store z values in array for mapping the image
-                % vv TODO plot data live
-                %image(C)
-                point = 50;
+                point = 60;
                 scatter(x, y, point, z, 'filled')
-                axis([5 85 5 85])
+                %axis([-20 20 -20 20])
                 hold on
                 drawnow
-                %timeout = 20;
             end
             pause(0.5)
             timeout = timeout + 1;
@@ -82,8 +80,6 @@ for phi = 10:2:80            %sweep through servo angles
 end
 shouldStop = true;
 end
-
-%image(C); %plots data as an image
 % remove keyboard handler to allow connection to the Arduino to clear
     set(f,'KeyPressFcn',@(a, b) 1);
     save(logfile, 'data');
